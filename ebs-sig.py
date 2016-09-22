869ad666d78bd39505f54a517d2c6b2cfdbda1d0
Thomas Bogevold <git@bogevold.com>
2016-09-22 10:17:50 +0200

    Lagt til signerings testkode

--- ebs-sig.py
+++ ebs-sig.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os
+import os, gnupg
 #Her havner produserte  filer /u01/sepa/ut/{SI – 01 -02 osv}
 #De som er tatt /u01/sepa/ut/{SI – 01 -02 osv}/tatt
 #Signeres til /u01/sg
+
+
 
 stiFra = "/u01/sepa/ut"
 stiTil = "/u01/sg"
@@ -17,3 +19,17 @@ for root, dirs, files in os.walk(stiFra):
       if nivaa == 5:
         print "Fra område: {0}".format(root)
         print "Filenavn:   {0}".format(file)
+
+
+# Testkode signering
+gpg = gnupg.GPG(gnupghome='/u01')
+gpg.encoding = 'utf-8'
+input_data = gpg.gen_key_input(key_type="RSA", key_length=1024)
+key = gpg.gen_key(input_data)
+stream = open('/u01/sepa/ut/SI/SI12345.xml', "rb")
+signed_data = gpg.sign_file(stream)
+f = open("/u01/test.xml.pgp", "w")
+f.write(str(signed_data))
+f.close()
+
+# www https://pythonhosted.org/python-gnupg/#signing
